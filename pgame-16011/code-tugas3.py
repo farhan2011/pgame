b1e65949acbfd8583a51756aa6ae0504d67ebda7
farhan2011 <56965521+farhan2011@users.noreply.github.com>
2019-10-25 00:35:17 +0900

    tugas3

---
+++ pgame-16011/code-tugas3.py
@@ -0,0 +1,10 @@
+# Tugas 3. Praktik Git untuk MK Pemrograman Game
+
+nama = "Nama 		: farhan tjan" 
+print (nama)
+npm = "NPM 		: 121055520116011" 
+print (npm)
+semester = "Semester 	: 7 (tujuh)" 
+print (semester)
+matakuliah = "Mata Kuliah 	: Pemograman Game" 
+print (matakuliah)
