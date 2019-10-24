27ad41c9bc2230cc72122ed56b875d6e5a0dde1e
farhan2011 <56965521+farhan2011@users.noreply.github.com>
2019-10-25 00:27:18 +0900

    tugas2

---
+++ pgame-16011/code-tugas2.py
@@ -0,0 +1,7 @@
+# Tugas 2. Praktik Git untuk MK Pemrograman Game
+
+print ("Hello, my friends") #Halo, teman-teman
+print ("my name is farhan tjan") #nama saya farhan tjan
+#baris ini tidak akan dieksekusi
+print ("Hallo, anak2 info ummu") #baris ini dieksekusi
+#Baris ini juga merupakan Baris Komentar 
